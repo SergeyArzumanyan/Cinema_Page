7c3ac770156346a8155f1b32a78692d4a6de8c40
Sergo <sergo.arzumanyan000@gmail.com>
2023-03-07 20:40:40 +0400

    changed footer layout

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
-@Component({
+@Component( {
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
-})
-export class FooterComponent {
+  styleUrls: [ './footer.component.scss' ]
+} )
 
+export class FooterComponent {
+  public screenIsLarge = ( window.innerWidth >= 650 );
+
+  private  toggleScreenState(): void {
+   this.screenIsLarge = ( window.innerWidth >= 650 );
+  }
+
+  @HostListener( 'window:resize', [ '$event.target' ] )
+  private onScreenSizeChange() {
+    this.toggleScreenState();
+  }
 }
