19106e4d987cfc54ab5e200a7a35e1e9dc4ef1ac
Sergey Arzumanyan <Sergey.Arzumanyan.work@gmail.com>
2024-02-12 19:04:27 +0400

    Added Firebase Hosting.

---
+++ src/environment.ts
@@ -0,0 +1,11 @@
+export const environment =  {
+  production: "true",
+  firebase: {
+    apiKey: "AIzaSyArtGwAFrVzABLCT6v895v4gmpuO3iO_R0",
+    authDomain: "cinema--page.firebaseapp.com",
+    projectId: "cinema--page",
+    storageBucket: "cinema--page.appspot.com",
+    messagingSenderId: "573598075067",
+    appId: "1:573598075067:web:b1351510d3b600337e419e"
+  },
+};
